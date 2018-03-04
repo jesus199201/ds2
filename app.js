911ea493f9865ab74c6458db5e849ab65fa67c8d
jesus199201 <jesus.199201@hotmail.com>
2018-03-03 19:38:23 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,77 +1,24 @@
-var app = angular.module('Movimiento', []);
-    app.controller("control", function ($scope, $http) {
-        $scope.movnom = "";
-        $scope.movtip="";
-        $scope.movvalor= "";
-    
-        
-        $scope.get = Function(){       
-        var movimiento=this;
-            
+ 
+  (function() {
+    var app = angular.module('movimiento', []);
+
+    app.controller('Controller', ['$http', function($http){
+      var datos = this;
+      datos.movimientos = [];
+
+      $http.get('http://www.titandesarrollo.com/_api/movimientos').success(function(data){
+       datos.movimientos = data;
        
-        
-        
-        
-        
-        
-        $scope.Save = function () {
-      
-        var httpreq = {
-                method: 'POST',
-                url: 'http://www.titandesarrollo.com/_api/movimientos',
-                headers: {
-                    'Content-Type': 'application/json; charset=utf-8',
-                    'dataType': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js'
-                },
-                data: { MovNom: $scope.movnom },
-                data: { MovTipo: $scope.movtip },
-                data: { MovVa: $scope.movvalor}
-            
-            }
-            $http(httpreq).success(function (response) {
-                $scope.fillList();
-                alert("Se Ha Ingresado a la tabla.");
-            })
-        };
-        /*
-        
-        $scope.Delete = function (SID) {
-            if (confirm("estas seguro de eliminar")) {
-                var httpreq = {
-                    method: 'POST',
-                    url: 'Default.aspx/Delete',
-                    headers: {
-                        'Content-Type': 'application/json; charset=utf-8',
-                        'dataType': 'json'
-                    },
-                    data: { StudentID: SID }
-                }
-                $http(httpreq).success(function (response) {
-                    $scope.fillList();
-                    alert("Deleted successfully.");
-                })
-            }
-        };
-        */
-        
-        $scope.fillList = function () {
-            $scope.movnom  = "";
-            $scope.movtip = "";
-            $scope.movvalorv= "";
-            var httpreq = {
-                method: 'POST',
-                url: 'http://www.titandesarrollo.com/_api/movimientos',
-                headers: {
-                    'Content-Type': 'application/json; charset=utf-8',
-                    'dataType': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js'
-                },
-                data: {}
-            }
-            $http(httpreq).success(function (response) {
-                $scope.movimientoList = response.d;
-            })
-        };
-        $scope.fillList();
+       });
+    }]);
+
+    app.controller('RController', function() {
+      this.review = {};
+
+      this.addReview = function(movimientos) {
+        movimientos.reviews.push(this.review);
+
+        this.review = {};
+      };
     });
-        
-        
+  })();
